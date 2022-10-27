4a7dbe28e0b5d6052824bfdb48d1114fac105f71
zobayertihan <zobayertihan@gmail.com>
2022-10-27 06:24:57 +0600

    FAQ updated

--- src/components/pages/FAQ/FAQ.js
+++ src/components/pages/FAQ/FAQ.js
@@ -4,7 +4,7 @@ const FAQ = () => {
     return (
         <div>
             <div className="bg-gray-100">
-                <h1>Frequently Asked Questions.</h1>
+                <h1 className='text-center font-bold text-2xl'>Frequently Asked Questions.</h1>
                 <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="relative grid gap-5 sm:grid-cols-2 lg:grid-cols-2">
                         <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
